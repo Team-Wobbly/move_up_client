9c37afdbffdd0493ccca33f6c83c1793100c3388
Jm <jmclagniez@gmail.com>
2016-10-01 18:03:54 +0200

    add glup task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
-var gulp = require('gulp');
+let gulp = require('gulp');
+let del = require('del');
+let typescript = require('gulp-typescript');
+let tscConfig = require('./tsconfig.json');
 
-gulp.task('default', function() {
-  
+gulp.task('clean', function () {
+  return del('build/**/*');
 });
+
+gulp.task('compile', ['clean'], function () {
+  console.log('Compiling TS files...');
+  return gulp
+    .src('app/**/*.ts')
+    .pipe(typescript(tscConfig.compilerOptions))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['compile']);
+gulp.task('default', ['build']);
