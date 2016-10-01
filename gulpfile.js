214635179657590973b56d9f4aa26937add5c83e
Jm <jmclagniez@gmail.com>
2016-10-02 00:44:48 +0200

    fix duplicate identifier

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,18 @@ let gulp = require('gulp');
 let del = require('del');
 let typescript = require('gulp-typescript');
 let tscConfig = require('./tsconfig.json');
+let tslint = require('gulp-tslint');
 
 gulp.task('clean', function () {
   return del('build/**/*');
+});
+
+gulp.task('tslint', ['clean'], function() {
+  return gulp.src('app/**/*.ts')
+    .pipe(tslint({
+        formatter: "verbose"
+    }))
+    .pipe(tslint.report())
 });
 
 gulp.task('compile', ['clean'], function () {
@@ -15,5 +24,10 @@ gulp.task('compile', ['clean'], function () {
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('build', ['compile']);
+gulp.task('copy:assets', ['clean'], function() {
+  //return gulp.src(['app/**/*', 'index.html', 'styles.css', '!app/**/*.ts'], { base : './' })
+    //.pipe(gulp.dest('build'))
+});
+
+gulp.task('build', ['compile', 'copy:assets']);
 gulp.task('default', ['build']);
